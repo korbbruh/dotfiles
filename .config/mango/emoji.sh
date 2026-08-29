88ef6d6540e8b5e36b86fd3abb20bea300f52cf8
korv <laceras.korb@gmail.com>
2026-08-29 13:35:24 +0800

    mango moved to nixos repo

--- .config/mango/emoji.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# ~/.config/mango/emoji.sh
-# Emoji picker via bemoji, using rofi + wl-copy. Copy-to-clipboard mode.
-#   yay -S bemoji wl-clipboard
-set -euo pipefail
-
-export BEMOJI_PICKER_CMD="rofi -dmenu -theme dmenu -p emoji"
-export BEMOJI_CLIP_CMD="wl-copy"
-bemoji
